22be08120115d021fa9ed844c90089f57e2c3641
Rachel Vanderloop <rvanderl@uwo.ca>
2021-05-19 23:52:00 -0400

    moved social media handles into contactvidsection

--- src/components/ContactVidSection.js
+++ src/components/ContactVidSection.js
@@ -9,6 +9,23 @@ function ContactVidSection() {
                 <h1>About Rachel</h1> 
                 <h1>pronouns: they/them</h1>
             </div>
+            <div className='upper-container'>
+                <div className='platform-container'>
+                    
+                    <div className='platform-clmn'>
+                        <i className='fab fa-facebook'/> <p className='handle'>facebook.com/rachel.vanderloop/</p>
+                        <i className='fab fa-instagram'/> <p className='handle'>@raynotrachel332</p>
+                    </div>
+                    <div className='platform-clmn'>
+                        <i className='fab fa-github'/> <p className='handle'>rachel-vanderloop</p>
+                        <i className='fab fa-linkedin'/> <p className='handle'>rachelvanderloop</p>
+                    </div>
+                    <div className='platform-clmn'>
+                        <i className='fas fa-envelope'/> <p className='handle'>rachelvanderloop@gmail.com</p>
+                        <i className='fas fa-envelope'/> <p className='handle'>rvanderl@uwo.ca</p>
+                    </div>                    
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,24 +8,7 @@ function Contact() {
     return (
         <div>
             <ContactVidSection/>
-            <div className='upper-container'>
-                <div className='platform-container'>
-                    
-                    <div className='platform-clmn'>
-                        <i className='fab fa-facebook'/> <p className='handle'>facebook.com/rachel.vanderloop/</p>
-                        <i className='fab fa-instagram'/> <p className='handle'>@raynotrachel332</p>
-                    </div>
-                    <div className='platform-clmn'>
-                        <i className='fab fa-github'/> <p className='handle'>rachel-vanderloop</p>
-                        <i className='fab fa-linkedin'/> <p className='handle'>rachelvanderloop</p>
-                    </div>
-                    <div className='platform-clmn'>
-                        <i className='fas fa-envelope'/> <p className='handle'>rachelvanderloop@gmail.com</p>
-                        <i className='fas fa-envelope'/> <p className='handle'>rvanderl@uwo.ca</p>
-                    </div>                    
-                </div>
-            </div>
-
+            
             <div className='lower-container'>
                 <div className='form-map-wrapper'>
                     <ContactForm/>
