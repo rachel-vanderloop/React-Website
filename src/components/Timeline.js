a533eeecac943c705d40a5a2e1157a8141523a39
Rachel Vanderloop <rvanderl@uwo.ca>
2022-03-22 20:31:59 -0400

    ROUGH start on the timeline section, used a very simple example from W3 so I can try playing with it from there!

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,9 +5,58 @@ import './Timeline.css';
 
 function Timeline() {
     return (
-        <div>
-
-        </div>
+        <>
+            <div class="timeline">
+                <div class="container left">
+                    <div class="content">
+                    <h2>2020</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                    <h2>2019</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container left">
+                    <div class="content">
+                    <h2>2018</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                    <h2>2017</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container left">
+                    <div class="content">
+                    <h2>2016</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                    <h2>2015</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container left">
+                    <div class="content">
+                    <h2>2013</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                    <h2>2012</h2>
+                    <p>Lorem ipsum..</p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/pages/Updates.js
+++ src/components/pages/Updates.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Updates.css';
 import '../../App.css';
 import Footer from '../Footer';
+import Timeline from '../Timeline';
 
 function Updates() {
     return (
         <>
-            <div>
-                <h1>TIMELINE</h1>
+            <div className='title-box'>
+                <div className='title-text'>
+                    <h1>UPDATES AND TIMELINE</h1>
+                    <p>stuff happens here teehee</p>
+                </div>
+                
             </div>
+            <Timeline/>
         </>
     )
 }
